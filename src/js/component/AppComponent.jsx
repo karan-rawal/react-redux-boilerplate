9713828d4697bfbd09be955c47ffed47d4b8fc7b
Karan Rawal <kr14nov@outlook.com>
2018-07-08 23:09:29 +0530

    Sourcemap enabled, support for assets like images and fonts, bootstrap 4 added.

--- src/js/component/AppComponent.jsx
+++ src/js/component/AppComponent.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import './AppComponent.scss';
+
+const image = require('../../assets/images/smiley.jpg');
 
 const AppComponent = () => (
-  <div>
-      App Component Rendered.
+  <div className="text-danger make-underline">
+      App Component Rendered. Test image:
+    <img src={image} alt="Smiley" />
   </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const SRC_PATH = path.resolve(__dirname, 'src');
 const DIST_PATH = path.resolve(__dirname, 'dist');
@@ -11,6 +12,7 @@ const config = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devtool: 'source-map',
   output: {
     filename: 'index.js',
     path: DIST_PATH,
@@ -27,12 +29,40 @@ const config = {
         test: /\.jsx$/,
         use: 'babel-loader',
       },
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          }],
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: 'file-loader?name=[name].[ext]&outputPath=assets/fonts/',
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: 'file-loader?name=[name].[ext]&outputPath=assets/images/',
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: `${SRC_PATH}/index.html`,
       filename: 'index.html',
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.css',
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
